703bd11930c3eab5a8027f765ab1ce5eae28abe5
Trungnc273 <160880795+Trungnc273@users.noreply.github.com>
2025-11-11 15:38:01 +0700

    fix order

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/auth");
 const ctrl = require("../controllers/orderController");
 
 router.post("/", auth, ctrl.createOrder); // create order (buyer)
-router.get("/id", auth, ctrl.getOrder);
+router.get("/:id", auth, ctrl.getOrder);
 router.get("/", auth, ctrl.listOrdersForUser); // ?userId or auth user
 
 module.exports = router;
